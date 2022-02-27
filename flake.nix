07cff2d476bde8225f63e431fe64ab4e11e45ea0
Ingo Ruhnke <grumbel@gmail.com>
2022-02-27 20:28:25 +0100

    Add Nix flake build files

---
+++ flake.nix
@@ -0,0 +1,29 @@
+rec {
+  description = "Windstille GUI Engine";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          fontgen = pkgs.stdenv.mkDerivation {
+            pname = "fontgen";
+            version = "0.0.0";
+            src = nixpkgs.lib.cleanSource ./.;
+            nativeBuildInputs = [
+              pkgs.cmake
+            ];
+            buildInputs = [
+              pkgs.freetype
+            ];
+           };
+        };
+        defaultPackage = packages.fontgen;
+      });
+}
