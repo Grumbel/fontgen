7ee68c49f62a8469e460cce72fe306664e07013a
Ingo Ruhnke <grumbel@gmail.com>
2025-11-16 03:16:49 +0100

    Update to nixos-25.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@ rec {
   description = "TTF to PNG Font Generator";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
